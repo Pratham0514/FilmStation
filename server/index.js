124e927f305cd598e49b75af29fdb67bf4c3d44a
Pratham0514 <prathameshroundale05@gmail.com>
2026-03-09 16:06:25 +0530

    setup of database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -9,14 +10,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    if (conn) {
+      console.log("\n 📦 Connected to MongoDB ");
+    }
+  } catch (error) {
+    console.log("MongoDB connection error ❌", error.message);
+  }
+};
+
 app.get("/", (req, res) => {
-    res.send({
-        status: "ok",
-        message: "Server is healthy",
-    });
+  res.send({
+    status: "ok",
+    message: "Server is healthy",
+  });
 });
-const PORT = process.env.PORT || 8080
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`\n ✅ Server is running on port ${PORT}`);
+  connectDB();
 });
