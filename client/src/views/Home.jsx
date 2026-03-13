a28184b8d8d66a87789ad62f088b2d256389393d
Pratham0514 <prathameshroundale05@gmail.com>
2026-03-13 17:53:13 +0530

    search parat complete

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,18 +1,54 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import MovieCard from "./../component/MovieCard"
+import toast, { Toaster } from 'react-hot-toast';
 function Home() {
-  const [movies, setMovies] = useState([])
+   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [error , setError] = useState("");
+  
 
-  const loadMovies =async ()=>{
-    const response =await axios.get("http://localhost:8080/movies");
+  // Load all movies
+  const loadMovies = async () => {
+    const response = await axios.get("http://localhost:8080/movies");
     setMovies(response.data.data);
-  }
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     loadMovies();
-  },[])
+  }, []);
+
+  // Search movies
+// Search movies
+const searchMovies = async () => {
+  try {
+    toast.loading("Searching...", { id: "searching" });
+
+    const response = await axios.get(
+      `http://localhost:8080/movies/search?title=${search}`
+    );
+
+    toast.dismiss("searching");
+
+    setMovies(response.data.data);
+    setError("");
+  } catch (error) {
+    toast.dismiss("searching");
+    toast.error(error.response.data.message || "Something went wrong" ,{ id: "searching" },{duration: 500});
+    setMovies([]);
+    setError(error.response.data.message || "Something went wrong");
+  }
+};
+
+useEffect(() => {
+  if (search.trim() !== "") {
+    searchMovies();
+  } else {
+    loadMovies();
+  }
+}, [search]);
   return (
-    <div className="min-h-screen bg-gradient-to-b from-black via-gray-900 to-black ">
+    <div className="min-h-screen bg-gradient-to-b from-black via-gray-900 to-black">
 
   <div className="container mx-auto px-6 py-10">
 
@@ -21,8 +57,21 @@ function Home() {
       🎥 Movies 🍿
     </h1>
 
+    {/* Search Box */}
+    {/* Search Input */}
+      <input
+        type="text"
+        placeholder="Search Movies"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="block bg-gray-800 text-white px-4 py-2 rounded-lg w-full md:w-1/2 lg:w-1/3 mb-10 focus:outline-none focus:ring-2 focus:ring-blue-500 mx-auto"
+      />
+    {error ? (
+      <p className="text-red-500 text-center text-2xl">{error}</p>
+    ) : null}
+
     {/* Movies Grid */}
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10">
 
       {movies.map((movieObj) => {
         const { _id, title, image, category, year, rating } = movieObj;
@@ -42,7 +91,7 @@ function Home() {
     </div>
 
   </div>
-
+        <Toaster />
 </div>
   )
 }
